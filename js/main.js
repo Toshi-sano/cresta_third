dfa766b84f08b980d5d740cf98358f3d2d6fec1d
Toshisano git <chargeman59@gmail.com>
2021-04-05 19:56:49 +0900

    ヘッダーの追従を実装

--- js/main.js
+++ js/main.js
@@ -2,12 +2,14 @@ $(function() {
   $('.menu-btn').on('click', function() {
     $(this).toggleClass('active');
     $('.menu-layer').toggleClass('active');
+    $('.header-flex').toggleClass('active');
     $('.black-layer').fadeToggle();
   });
 
   $('.black-layer').on('click', function(){
     $('.menu-btn').toggleClass('active');
     $('.menu-layer').toggleClass('active');
+    $('.header-flex').toggleClass('active');
     $(this).fadeToggle();
   });
 });
